7d76c0682d177d961f5482d41a12be21097a3409
tomohisa-osada <agekuta@gmail.com>
2017-11-01 19:38:47 +0900

    add line expansion

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # 問題 & TODO
 - [ ] svgeditor-* classの出力からの消去ができない（dragTargetがundefinedにならなくなる？）
 - [ ] ellipse の rx = ry のとき circle にする
+- [ ] 拡大用頂点で反転させるとおかしい
--- src/preview/svgutils.ts
+++ src/preview/svgutils.ts
@@ -5,32 +5,39 @@ class SvgDeformer {
   constructor(public elem: SVGElement) {
   }
 
+  geta(name: string): string {
+    return this.elem.getAttribute(name);
+  }
+  seta(name: string, value: string): void {
+    return this.elem.setAttribute(name, value);
+  }
+
   setPosition(point: Point): void {
     switch (this.elem.tagName) {
       case "circle":
       case "ellipse":
-        this.elem.setAttribute("cx", String(point.x));
-        this.elem.setAttribute("cy", String(point.y));
+        this.seta("cx", String(point.x));
+        this.seta("cy", String(point.y));
         break;
       case "rect":
       case "text":
-        this.elem.setAttribute("x", String(point.x));
-        this.elem.setAttribute("y", String(point.y));
+        this.seta("x", String(point.x));
+        this.seta("y", String(point.y));
         break;
       case "line":
-        let deltaX = +this.elem.getAttribute("x2") - +this.elem.getAttribute("x1");
-        let deltaY = +this.elem.getAttribute("y2") - +this.elem.getAttribute("y1");
-        this.elem.setAttribute("x1", String(point.x));
-        this.elem.setAttribute("y1", String(point.y));
-        this.elem.setAttribute("x2", String(point.x + deltaX));
-        this.elem.setAttribute("y2", String(point.y + deltaY));
+        let deltaX = +this.geta("x2") - +this.geta("x1");
+        let deltaY = +this.geta("y2") - +this.geta("y1");
+        this.seta("x1", String(point.x));
+        this.seta("y1", String(point.y));
+        this.seta("x2", String(point.x + deltaX));
+        this.seta("y2", String(point.y + deltaY));
         break;
       case "polyline":
       case "polygon":
-        let points = parsePoints(this.elem.getAttribute("points"));
+        let points = parsePoints(this.geta("points"));
         let delta = point.sub(points[0]);
         let pointsProperty = points.map(p => p.add(delta)).map(p => p.x + "," + p.y).join(" ");
-        this.elem.setAttribute("points", pointsProperty);
+        this.seta("points", pointsProperty);
         break;
       default:
         throw `not defined SVGElement: ${this.elem.tagName}`;
@@ -42,23 +49,23 @@ class SvgDeformer {
       case "circle":
       case "ellipse":
         return Point.of(
-          +this.elem.getAttribute("cx"),
-          +this.elem.getAttribute("cy")
+          +this.geta("cx"),
+          +this.geta("cy")
         );
       case "rect":
       case "text":
         return Point.of(
-          +this.elem.getAttribute("x"),
-          +this.elem.getAttribute("y")
+          +this.geta("x"),
+          +this.geta("y")
         );
       case "line":
         return Point.of(
-          +this.elem.getAttribute("x1"),
-          +this.elem.getAttribute("y1")
+          +this.geta("x1"),
+          +this.geta("y1")
         )
       case "polyline":
       case "polygon":
-        let points = parsePoints(this.elem.getAttribute("points"));
+        let points = parsePoints(this.geta("points"));
         return points[0];
       default:
         throw `not defined SVGElement: ${this.elem.tagName}`;
@@ -71,25 +78,34 @@ class SvgDeformer {
    * @param vertexes Recycled vertex nodes
    */
   setExpandVertexes(parentElement: HTMLElement, vertexes?: SVGElement[]): string[] {
-    // n : [0, 3], m : [0, 3]. undefinedなら飛ばす
+    // n : [0, 3], m : [0, 3]
     let frameFn: (n: number, m: number) => Point;
     let center: Point;
+    let leftUp: Point;
+    let width: number;
+    let height: number;
     switch (this.elem.tagName) {
       case "circle":
         center = this.getPosition();
-        let r = +this.elem.getAttribute("r");
+        let r = +this.geta("r");
         frameFn = (n, m) => Point.of(center.x + r * (n - 1), center.y + r * (m - 1));
         break;
       case "ellipse":
         center = this.getPosition();
-        let rx = +this.elem.getAttribute("rx");
-        let ry = +this.elem.getAttribute("ry");
+        let rx = +this.geta("rx");
+        let ry = +this.geta("ry");
         frameFn = (n, m) => Point.of(center.x + rx * (n - 1), center.y + ry * (m - 1));
         break;
       case "rect":
-        let leftUp = this.getPosition();
-        let width = +this.elem.getAttribute("width");
-        let height = +this.elem.getAttribute("height");
+        leftUp = this.getPosition();
+        width = +this.geta("width");
+        height = +this.geta("height");
+        frameFn = (n, m) => Point.of(leftUp.x + width * n / 2, leftUp.y + height * m / 2);
+        break;
+      case "line":
+        leftUp = Point.of(Math.min(+this.geta("x1"), +this.geta("x2")), Math.min(+this.geta("y1"), +this.geta("y2")));
+        width = Math.abs(+this.geta("x1") - +this.geta("x2"));
+        height = Math.abs(+this.geta("y1") - +this.geta("y2"));
         frameFn = (n, m) => Point.of(leftUp.x + width * n / 2, leftUp.y + height * m / 2);
         break;
       default:
@@ -172,6 +188,26 @@ class SvgDeformer {
           }
         );
         break;
+      case "line":
+        dirSwitch(direction,
+          () => {
+            let left = +this.geta("x1") < +this.geta("x2") ? "x1" : "x2";
+            this.add(left, delta);
+          },
+          () => {
+            let right = +this.geta("x1") > +this.geta("x2") ? "x1" : "x2";
+            this.add(right, delta);
+          },
+          () => {
+            let up = +this.geta("y1") < +this.geta("y2") ? "y1" : "y2";
+            this.add(up, delta);
+          },
+          () => {
+            let down = +this.geta("y1") > +this.geta("y2") ? "y1" : "y2";
+            this.add(down, delta);
+          }
+        );
+        break;
       default:
         throw `not defined SVGElement: ${this.elem.tagName}`;
     }
@@ -181,7 +217,7 @@ class SvgDeformer {
    * Add `delta` at the attribute `attr` of this element.
    */
   add(attr: string, delta: number): void {
-    this.elem.setAttribute(attr, String(+this.elem.getAttribute(attr) + delta));
+    this.seta(attr, String(+this.geta(attr) + delta));
   }
 
   /**
@@ -189,10 +225,10 @@ class SvgDeformer {
    */
   addTransform(tfn: TransformFn): void {
     if (this.elem.hasAttribute("transform")) {
-      let attr = this.elem.getAttribute("transform");
-      this.elem.setAttribute("transform", `${attr} ${tfn.kind}(${tfn.args.join(" ")})`);
+      let attr = this.geta("transform");
+      this.seta("transform", `${attr} ${tfn.kind}(${tfn.args.join(" ")})`);
     } else {
-      this.elem.setAttribute("transform", `${tfn.kind}(${tfn.args.join(" ")})`);
+      this.seta("transform", `${tfn.kind}(${tfn.args.join(" ")})`);
     }
   }
 
@@ -202,22 +238,22 @@ class SvgDeformer {
    */
   addTransform2(tfn: TransformFn): void {
     if (this.elem.hasAttribute("transform")) {
-      let attr = this.elem.getAttribute("transform");
+      let attr = this.geta("transform");
       let transforms = this.getTransformAttrs();
       if (transforms[transforms.length - 1].kind === tfn.kind) {
         transforms[transforms.length - 1] = tfn;
-        this.elem.setAttribute("transform", transforms.map(tfn => `${tfn.kind}(${tfn.args.join(" ")})`).join(" "));
+        this.seta("transform", transforms.map(tfn => `${tfn.kind}(${tfn.args.join(" ")})`).join(" "));
       } else {
-        this.elem.setAttribute("transform", `${attr} ${tfn.kind}(${tfn.args.join(" ")})`);
+        this.seta("transform", `${attr} ${tfn.kind}(${tfn.args.join(" ")})`);
       }
     } else {
-      this.elem.setAttribute("transform", `${tfn.kind}(${tfn.args.join(" ")})`);
+      this.seta("transform", `${tfn.kind}(${tfn.args.join(" ")})`);
     }
   }
 
   getTransformAttrs(): TransformFn[] {
     if (this.elem.hasAttribute("transform")) {
-      let attr = this.elem.getAttribute("transform");
+      let attr = this.geta("transform");
       return parseTransform(attr);
     } else {
       return undefined;
