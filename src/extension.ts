8b47c963a2600955d95f91b0e2240f4917699fa7
henoc <agekutar@gmail.com>
2018-01-21 20:03:17 +0900

    ver up

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,7 @@ import * as fs from "fs";
 import * as path from "path";
 import * as yaml from "js-yaml";
 import {render} from "ejs";
-let SvgoConstructor = require("svgo");
+import * as SvgoConstructor from "svgo";
 
 export function activate(context: vscode.ExtensionContext) {
 
