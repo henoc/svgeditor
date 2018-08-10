14d946cc72e037ce27303ead4aec5be8dda0db3f
Henoc <agekutar@gmail.com>
2018-08-10 23:30:52 +0900

    fix unit problems

--- src/measureUnits.ts
+++ src/measureUnits.ts
@@ -1,9 +1,8 @@
 import { Length, LengthUnit, ParsedElement } from "./domParser";
 import { svgVirtualMap, svgRealMap } from "./main";
 import { assertNever, map, v, Vec2 } from "./utils";
-import { Assoc } from "./svg";
-
-const dpi = getCreatedElementDimensions(document.body, {width: "1in"}).x;
+import { SetDifference } from "utility-types";
+import memoize from "fast-memoize";
 
 /**
  * SVG unit converter
@@ -11,41 +10,19 @@ const dpi = getCreatedElementDimensions(document.body, {width: "1in"}).x;
 export function convertToPixel(length: Length, uuid: string): number {
     const re = svgRealMap[uuid];
     const attrKind = /^width$|^.?x$/.test(length.attrName) ? "horizontal" : "vertical";
-    let basePx: number | null;
+    const font = getComputedStyle(re).font || "";
 
     switch (length.unit) {
         case null:
         return length.value;
 
         case "%":
-        basePx = getSvgBasePx(uuid, attrKind);
+        let basePx = getSvgBasePx(uuid, attrKind);
         return basePx && (basePx * length.value / 100) || 400;
         // regard "%" length of outermost svg as fixed value, 400px
 
-        case "cm":
-        return length.value / 2.54 * dpi;
-
-        case "em":
-        const fsize =  parseFloat(getComputedStyle(re).fontSize /* ??px */ || "16");
-        return length.value * fsize;
-
-        case "ex":
-        return length.value * getCreatedElementDimensions(document.body, {font: getComputedStyle(re).font || ""}, "x").y;
-
-        case "in":
-        return length.value * dpi;
-
-        case "mm":
-        return length.value / 2.54 * dpi / 10;
-
-        case "pc":
-        return length.value * ((dpi / 72) * 12);
-
-        case "pt":
-        return length.value * dpi / 72;
-
-        case "px":
-        return length.value;
+        default:
+        return length.value * measure(font, length.unit);
     }
 }
 
@@ -55,11 +32,10 @@ export function convertToPixel(length: Length, uuid: string): number {
  */
 export function convertFromPixel(length: Length, targetUnit: LengthUnit, uuid: string): Length {
     const re = svgRealMap[uuid];    
+    const font = getComputedStyle(re).font || "";
     const attrKind = /^width$|^.?x$/.test(length.attrName) ? "horizontal" : "vertical";
-    let basePx: number | null;
     switch (targetUnit) {
         case null:
-        basePx = getSvgBasePx(uuid, attrKind);
         return {
             unit: null,
             value: length.value,
@@ -67,65 +43,19 @@ export function convertFromPixel(length: Length, targetUnit: LengthUnit, uuid: s
         };
 
         case "%":
-        basePx = getSvgBasePx(uuid, attrKind);
+        let basePx = getSvgBasePx(uuid, attrKind);
         return {
             unit: "%",
             attrName: length.attrName,
             value: basePx ? length.value / basePx * 100 : 100
         };
 
-        case "cm":
+        default:
         return {
-            unit: "cm",
+            unit: targetUnit,
             attrName: length.attrName,
-            value: length.value / dpi * 2.54
-        };
-
-        case "em":
-        const fsize =  parseFloat(getComputedStyle(re).fontSize /* ??px */ || "16");
-        return {
-            unit: "em",
-            attrName: length.attrName,
-            value: length.value / fsize
-        };
-
-        case "ex":
-        return {
-            unit: "ex",
-            attrName: length.attrName,
-            value: length.value / getCreatedElementDimensions(document.body, {font: getComputedStyle(re).font || ""}, "x").y
-        };
-
-        case "in":
-        return {
-            unit: "in",
-            attrName: length.attrName,
-            value: length.value / dpi
-        };
-
-        case "mm":
-        return {
-            unit: "mm",
-            attrName: length.attrName,
-            value: length.value * 2.54 / dpi * 10
-        };
-
-        case "pc":
-        return {
-            unit: "pc",
-            attrName: length.attrName,
-            value: length.value / ((dpi / 72) * 12)
-        };
-
-        case "pt":
-        return {
-            unit: "pt",
-            attrName: length.attrName,
-            value: length.value * 72 / dpi
-        };
-
-        case "px":
-        return length;
+            value: length.value / measure(font, targetUnit)
+        }
     }
 }
 
@@ -149,17 +79,20 @@ function getSvgBasePx(uuid: string, attrKind: "horizontal" | "vertical"): number
     return null;
 }
 
-function getCreatedElementDimensions(parent: Element, styleProps: Assoc, content?: string): Vec2 {
+function measureOneUnitLength(font: string, unit: SetDifference<LengthUnit, "%" | null>): number {
     const div = document.createElement("div");
     div.style.position = "absolute";
     div.style.zIndex = "-2147483648";
     div.style.left = "0";
     div.style.top = "0";
     div.style.visibility = "hidden";
-    Object.assign(div.style, styleProps);
-    if (content) div.innerHTML = content;
-    parent.insertAdjacentElement("beforeend", div);
-    const ret = v(div.offsetWidth, div.offsetHeight);
-    parent.removeChild(div);
+    div.style.width = `1${unit}`;
+    div.style.font = font;
+    document.body.insertAdjacentElement("beforeend", div);
+    const ret = parseFloat(getComputedStyle(div).width || "1");
+    document.body.removeChild(div);
     return ret;
 }
+
+const measure = memoize(measureOneUnitLength);
+
